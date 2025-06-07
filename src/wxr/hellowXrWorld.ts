fd18f9bf18d0909d81a663ae75d29499f5dc6f96
Matt Taylor <matt@matthewjosephtaylor.com>
2025-06-07 14:15:10 -0500

    cleaned up hellowXrWorld code

--- src/wxr/hellowXrWorld.ts
+++ src/wxr/hellowXrWorld.ts
@@ -9,39 +9,23 @@ import { createWebXrExperience } from "./createWebXrExperience";
 
 export const helloXrWorld = async (engine: BabEngine = createWebGlEngine()) => {
   const scene = Scenes.createScene(engine);
-  // var camera = new BABYLON.FreeCamera(
-  //   "camera1",
-  //   new BABYLON.Vector3(0, 5, -10),
-  //   scene
-  // );
-
   const camera = Cameras.getFreeCamera(scene, "camera1", {
     position: [0, 5, -10],
     target: [0, 0, 0],
   });
-  // camera.setTarget(BABYLON.Vector3.Zero());
   const canvas = engine.getRenderingCanvas();
   camera.attachControl(canvas, true);
-  // var light = new BABYLON.HemisphericLight(
-  //   "light1",
-  //   new BABYLON.Vector3(0, 1, 0),
-  //   scene
-  // );
   const light = Lights.getHemisphericLight(scene, "light1", {
     direction: [0, 1, 0],
     intensity: 0.7,
   });
-  // light.intensity = 0.7;
-  // var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
 
   const sphere = Meshes.getSphere(scene, "sphere", {
     radius: 1,
     position: [0, 1, 0],
   });
-  // sphere.position.y = 1;
 
   const env = createDefaultEnvironment(scene);
-  // const env = scene.createDefaultEnvironment();
   if (!env?.ground) {
     throw new Error("Failed to create default environment");
   }
@@ -50,10 +34,6 @@ export const helloXrWorld = async (engine: BabEngine = createWebGlEngine()) => {
     floorMeshes: [env.ground],
   });
 
-  // const xr = await scene.createDefaultXRExperienceAsync({
-  //   floorMeshes: [env.ground],
-  // });
-  // run the main render loop
   engine.runRenderLoop(() => {
     scene.render();
   });
