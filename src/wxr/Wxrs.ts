d11cc9d7adbb042c3f897abcb9aa2996e94c7a5e
Matt Taylor <matt@matthewjosephtaylor.com>
2025-06-07 14:01:26 -0500

    why is hellowXrWorld undefined, wtf

--- src/wxr/Wxrs.ts
+++ src/wxr/Wxrs.ts
@@ -2,10 +2,12 @@ import { createWebXrSessionManager } from "./createWebXrSessionManager";
 import { helloXrWorld } from "./hellowXrWorld";
 import { createWebXrExperience } from "./createWebXrExperience";
 import { createDefaultEnvironment } from "./createDefaultHelper";
+import { wtf } from "./wtf";
 
 export const Wxrs = {
   createWebXrSessionManager,
   helloXrWorld,
   createDefaultEnvironment,
   createWebXrExperience,
+  wtf,
 };
---
+++ src/wxr/wtf.ts
@@ -0,0 +1,5 @@
+
+export const wtf = () => {
+
+  console.log("wtf");
+};
