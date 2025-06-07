214281e961ba3a339aa6d624685ab7f8be1c6bdd
Matt Taylor <matt@matthewjosephtaylor.com>
2025-06-07 14:13:07 -0500

    removed wtf test function

--- src/wxr/Wxrs.ts
+++ src/wxr/Wxrs.ts
@@ -1,13 +1,11 @@
+import { createDefaultEnvironment } from "./createDefaultHelper";
+import { createWebXrExperience } from "./createWebXrExperience";
 import { createWebXrSessionManager } from "./createWebXrSessionManager";
 import { helloXrWorld } from "./hellowXrWorld";
-import { createWebXrExperience } from "./createWebXrExperience";
-import { createDefaultEnvironment } from "./createDefaultHelper";
-import { wtf } from "./wtf";
 
 export const Wxrs = {
   createWebXrSessionManager,
   helloXrWorld,
   createDefaultEnvironment,
   createWebXrExperience,
-  wtf,
 };
